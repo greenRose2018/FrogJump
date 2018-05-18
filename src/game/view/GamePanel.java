7d07c07f67ac775cccbdb66baa3f2534a90b8450
greenRose2018 <brifun13@gmail.com>
2018-05-18 09:35:38 -0600

    addded private data members

--- src/game/view/GamePanel.java
+++ src/game/view/GamePanel.java
@@ -9,7 +9,16 @@ import game.controller.GameController;
 
 public class GamePanel extends JPanel
 {
-	public GamePanel()
+	private GameController appController;
+	private SpringLayout layout;
+	private JLayeredPane panelContainer;
+	
+	private JButton onePlayer;
+	private JButton twoPlayers;
+	private JLabel gameImage;
+	
+	
+	public GamePanel(GameController appController)
 	{
 		
 	}
