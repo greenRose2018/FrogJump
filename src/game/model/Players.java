bb155514540539f0e639993f4467fd0270c59fd5
greenRose2018 <brifun13@gmail.com>
2018-05-17 23:00:11 -0600

    added gettters/setters to Players

--- src/game/model/Players.java
+++ src/game/model/Players.java
@@ -7,8 +7,61 @@ public class Players
 	private int attack;
 	private int level;
 	
-	public Players()
+	public Players(String name)
 	{
-		
+		this.username = name;
+		this.health = 10;
+		this.attack = 2;
+		level = 0;
 	}
+	
+	public String getUsername()
+	{
+		return username;
+	}
+
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+
+	public int getHealth()
+	{
+		return health;
+	}
+
+	public void setHealth(int health)
+	{
+		this.health = health;
+	}
+
+	public int getAttack()
+	{
+		return attack;
+	}
+
+	public void setAttack(int attack)
+	{
+		this.attack = attack;
+	}
+
+	public int getLevel()
+	{
+		return level;
+	}
+
+	public void setLevel(int level)
+	{
+		this.level = level;
+	}
+	
+	public String toString()
+	{
+		String str = "";
+		str += "this is the name of the player one: " + getUsername() + "\n has the health of: " + getHealth() + " \n with an attack of: " + getAttack();
+		return str;
+	}
+	
+	
+	
 }
