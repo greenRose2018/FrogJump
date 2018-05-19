13f4ece33573d17cb93acd37d014dcbab6dd7408
greenRose2018 <brifun13@gmail.com>
2018-05-19 13:02:24 -0600

    called game frame to controller to test

--- src/game/controller/GameController.java
+++ src/game/controller/GameController.java
@@ -5,13 +5,20 @@ import game.view.*;
 
 public class GameController
 {
+	private GameFrame gameFrame;
 	
 	public GameController()
 	{
-		
+		gameFrame = new GameFrame(this);
 	}
 	public void Start()
 	{
 		
 	}
+	
+	public GameFrame getGameFrame()
+	{
+		return gameFrame;
+	}
+	
 }
