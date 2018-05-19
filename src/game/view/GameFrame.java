4b92837baaf1df860a2bf3bad72cd9d1fd7f716b
greenRose2018 <brifun13@gmail.com>
2018-05-19 13:00:21 -0600

    added to gameFrame

--- src/game/view/GameFrame.java
+++ src/game/view/GameFrame.java
@@ -1,16 +1,40 @@
 package game.view;
 
+import game.controller.*;
 import javax.swing.*;
 
 public class GameFrame extends JFrame
 {
-	public GameFrame()
+	private GameController appController;
+	private GamePanel gamePanel;
+	
+	public GameFrame(GameController appController)
 	{
+		super();
+		this.appController = appController;
+		gamePanel = new GamePanel(appController);
+		setupFrame();
 		
 	}
 	
 	public void setupFrame()
 	{
-		
+		this.setContentPane(gamePanel);
+		this.setSize(600, 600);
+		this.setTitle("Frog Jump");
+		this.setResizable(false);
+		this.setVisible(true);
 	}
+	
+	public GameController getAppController() {
+		return appController;
+	}
+
+	public void setAppController(GameController appController) {
+		this.appController = appController;
+	}
+
+	
+	
+	
 }
