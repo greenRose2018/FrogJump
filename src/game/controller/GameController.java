82a83677ccc50b8dcd8ca04e5536392b1c950a40
greenRose2018 <brifun13@gmail.com>
2018-05-22 10:05:02 -0600

    tested frog

--- src/game/controller/GameController.java
+++ src/game/controller/GameController.java
@@ -2,6 +2,7 @@ package game.controller;
 
 import game.model.*;
 import game.view.*;
+import javax.swing.JFrame;
 
 public class GameController
 {
@@ -9,11 +10,16 @@ public class GameController
 	
 	public GameController()
 	{
-		gameFrame = new GameFrame(this);
+		//gameFrame = new GameFrame(this);
 	}
 	public void Start()
 	{
-		
+		JFrame f = new JFrame();
+		Frog frog = new Frog();
+		f.add(frog);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(800, 600);
 	}
 	
 	public GameFrame getGameFrame()
