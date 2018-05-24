0c499ec1775aeb04b4d20f70937d452e5b653122
greenRose2018 <brifun13@gmail.com>
2018-05-23 22:30:06 -0600

    created a new frame

---
+++ src/game/view/GameFrame.java
@@ -0,0 +1,23 @@
+package game.view;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame
+{	
+	private Frog frog;
+
+	public GameFrame()
+	{
+		super();
+		frog = new Frog();
+		setupFrame();
+	}
+	public void setupFrame()
+	{
+		this.add(frog);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(800, 600);
+	}
+
+}
