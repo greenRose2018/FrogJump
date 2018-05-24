3c21bdbf54ed8b8132ac7fa005dbab96c18df5af
greenRose2018 <brifun13@gmail.com>
2018-05-24 09:11:22 -0600

    created new panel for gamepanel

---
+++ src/game/view/GamePanel.java
@@ -0,0 +1,27 @@
+package game.view;
+
+import javax.swing.*;
+
+public class GamePanel extends JPanel
+{
+	private JPanel options;
+	
+	public GamePanel()
+	{
+		options = new JPanel();
+		
+	}
+	public void setupListeners()
+	{
+		
+		
+	}
+	public void setupPanel()
+	{
+		this.add(options);
+	}
+	public void setupLayout()
+	{
+		
+	}
+}
